a199abb068b8db87c4c3c78a7f4c76a4f0f59112
Jonathan Williams <jonathanwilliams96@hotmail.com>
2019-04-23 10:38:08 -0400

    update

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,6 +2,11 @@ import React, {Component} from 'react';
 import '../css/Book.css';
 import PropTypes from 'prop-types';
 
+const GENERIC_BOOK_TITLE = "New Book Title",
+  GENERIC_BOOK_AUTHOR = "New Book Author",
+  GENERIC_BOOK_YEAR = "Year",
+  GENERIC_BOOK_GENRE = "Genre";
+
 class Book extends Component {
     constructor() {
         super();
@@ -10,29 +15,56 @@ class Book extends Component {
     componentWillMount(){
         
         this.state = {
-            title: this.props.title,
-            author: this.props.author,
-            year: this.props.year,
-            genre: this.props.genre,
-            readMode: false
+            title: GENERIC_BOOK_TITLE,
+            author: GENERIC_BOOK_AUTHOR,
+            year: GENERIC_BOOK_YEAR,
+            genre: GENERIC_BOOK_GENRE,
+            readMode: false,
+            editMode: false,
+            added: false
         }
     }
 
     handleRead(){
         this.setState(
-        {
-             readMode: true
-        });
+            {
+                readMode: true,
+                
+            }
+        );
     }
 
-    handlerSave() {
+    handleEdit() {
         this.setState(
             {
-                readMode: true
-            
+          
+                editMode: true,
+                
+            }
+        );
+      }
+
+    handleSave() {
+        this.setState(
+            {
+                readMode: true,
+                
             }
         )
             
+    }
+
+    handleAdd() {
+        this.setState(
+            {
+                title: this.refs.titleContent.value,
+                author: this.refs.authorContent.value,
+                year: this.refs.yearContent.value,
+                genre: this.refs.genreContent.value,
+                editMode: false,                
+                added: true
+            }
+        );
     }
 
     render() {
@@ -41,17 +73,78 @@ class Book extends Component {
         authorElement = this.state.author;
         yearElement = this.state.year;
         genreElement = this.state.genre;
-        if(this.state.readMode) {
-            buttonArea = <div><button className="btn btn-danger" >Read</button></div> ;
-        } else {
+
+        // if(this.state.readMode && !this.state.editMode) {
+        //     buttonArea = <div><button className="btn btn-danger" >Read</button></div> ;
+        // } else {
+        //     buttonArea = <div><button className="btn btn-warning" onClick={this.handleRead.bind(this)}>Unread</button></div>;
+        // }
+
+        if(!this.state.added && !this.state.editMode) {
+            titleElement = <h5>{this.state.title}</h5>
+            authorElement = <p>{this.state.author}</p>
+            yearElement = <p>{this.state.year}</p>
+            genreElement = <p>{this.state.genre}</p>
+            buttonArea = (
+                <div>
+                    <button className="btn btn-primary" onClick={this.handleEdit.bind(this)}>Edit
+                    </button>
+            </div> )
+        }   else if(this.state.editMode) {
+            titleElement = (
+                <textarea
+                  ref="titleContent"
+                  className="title-textarea"
+                  defaultValue={this.state.title}
+                />
+              );
+              authorElement = (
+                <textarea
+                  ref="authorContent"
+                  className="author-textarea"
+                  defaultValue={this.state.author}
+                />
+              );
+              yearElement = (
+                <textarea
+                  ref="yearContent"
+                  className="year-textarea"
+                  defaultValue={this.state.year}
+                />
+              );
+              genreElement = (
+                <textarea
+                  ref="genreContent"
+                  className="genre-textarea"
+                  defaultValue={this.state.genre}
+                />
+              );
+            buttonArea = (
+                <div>
+                    <button className="btn btn-danger" onClick={this.handleAdd.bind(this)}>Add to Bookshelve
+                    </button>
+                </div> 
+            );
+        }   else if (this.state.readMode) {
+            titleElement = <h5>{this.state.title}</h5>
+            authorElement = <p>{this.state.author}</p>
+            yearElement = <p>{this.state.year}</p>
+            genreElement = <p>{this.state.genre}</p>
+            buttonArea = (
+                <div>
+                    <button className="btn btn-danger" onClick={this.handleSave.bind(this)}>Read
+                    </button>
+            </div> )
+        }   else  {
             buttonArea = <div><button className="btn btn-warning" onClick={this.handleRead.bind(this)}>Unread</button></div>;
         }
+        
         
         return (
             <div className="col-sm-2">
             <div className="card card-view text-white bg-info">
             <div className="card-body ">
-               <p>{titleElement}</p>
+               <h5>{titleElement}</h5>
                <p>{authorElement}</p>
                <p>{yearElement}</p>
                <p>{genreElement}</p>
