7b38ddc6b6a37ba4c243a28eecb4b65f6c9aba95
Fah P <34889621+ppariya@users.noreply.github.com>
2019-04-23 10:27:16 -0400

    Add files via upload
    adding/editing book function

---
+++ src/Book.js
@@ -0,0 +1,175 @@
+import React, {Component} from 'react';
+import '../css/Book.css';
+import PropTypes from 'prop-types';
+
+const GENERIC_BOOK_TITLE = "New Book Title",
+  GENERIC_BOOK_AUTHOR = "New Book Author",
+  GENERIC_BOOK_YEAR = "Year",
+  GENERIC_BOOK_GENRE = "Genre";
+
+class Book extends Component {
+    constructor() {
+        super();
+    }
+
+    componentWillMount(){
+        
+        this.state = {
+            title: GENERIC_BOOK_TITLE,
+            author: GENERIC_BOOK_AUTHOR,
+            year: GENERIC_BOOK_YEAR,
+            genre: GENERIC_BOOK_GENRE,
+            readMode: false,
+            editMode: false,
+            added: false
+        }
+    }
+
+    handleRead(){
+        this.setState(
+            {
+                readMode: true,
+                
+            }
+        );
+    }
+
+    handleEdit() {
+        this.setState(
+            {
+          
+                editMode: true,
+                
+            }
+        );
+      }
+
+    handleSave() {
+        this.setState(
+            {
+                readMode: true,
+                
+            }
+        )
+            
+    }
+
+    handleAdd() {
+        this.setState(
+            {
+                title: this.refs.titleContent.value,
+                author: this.refs.authorContent.value,
+                year: this.refs.yearContent.value,
+                genre: this.refs.genreContent.value,
+                editMode: false,                
+                added: true
+            }
+        );
+    }
+
+    render() {
+        let titleElement, authorElement, yearElement, genreElement, buttonArea;
+        titleElement = this.state.title;
+        authorElement = this.state.author;
+        yearElement = this.state.year;
+        genreElement = this.state.genre;
+
+        // if(this.state.readMode && !this.state.editMode) {
+        //     buttonArea = <div><button className="btn btn-danger" >Read</button></div> ;
+        // } else {
+        //     buttonArea = <div><button className="btn btn-warning" onClick={this.handleRead.bind(this)}>Unread</button></div>;
+        // }
+
+        if(!this.state.added && !this.state.editMode) {
+            titleElement = <h5>{this.state.title}</h5>
+            authorElement = <p>{this.state.author}</p>
+            yearElement = <p>{this.state.year}</p>
+            genreElement = <p>{this.state.genre}</p>
+            buttonArea = (
+                <div>
+                    <button className="btn btn-primary" onClick={this.handleEdit.bind(this)}>Edit
+                    </button>
+            </div> )
+        }   else if(this.state.editMode) {
+            titleElement = (
+                <textarea
+                  ref="titleContent"
+                  className="title-textarea"
+                  defaultValue={this.state.title}
+                />
+              );
+              authorElement = (
+                <textarea
+                  ref="authorContent"
+                  className="author-textarea"
+                  defaultValue={this.state.author}
+                />
+              );
+              yearElement = (
+                <textarea
+                  ref="yearContent"
+                  className="year-textarea"
+                  defaultValue={this.state.year}
+                />
+              );
+              genreElement = (
+                <textarea
+                  ref="genreContent"
+                  className="genre-textarea"
+                  defaultValue={this.state.genre}
+                />
+              );
+            buttonArea = (
+                <div>
+                    <button className="btn btn-danger" onClick={this.handleAdd.bind(this)}>Add to Bookshelve
+                    </button>
+                </div> 
+            );
+        }   else if (this.state.readMode) {
+            titleElement = <h5>{this.state.title}</h5>
+            authorElement = <p>{this.state.author}</p>
+            yearElement = <p>{this.state.year}</p>
+            genreElement = <p>{this.state.genre}</p>
+            buttonArea = (
+                <div>
+                    <button className="btn btn-danger" onClick={this.handleSave.bind(this)}>Read
+                    </button>
+            </div> )
+        }   else  {
+            buttonArea = <div><button className="btn btn-warning" onClick={this.handleRead.bind(this)}>Unread</button></div>;
+        }
+        
+        
+        return (
+            <div className="col-sm-2">
+            <div className="card card-view text-white bg-info">
+            <div className="card-body ">
+               <h5>{titleElement}</h5>
+               <p>{authorElement}</p>
+               <p>{yearElement}</p>
+               <p>{genreElement}</p>
+               <p>{buttonArea}</p>
+            </div>
+            </div>
+            </div>
+
+
+        )
+    }
+}
+
+Book.defaultProps = { 
+    title: "Title",
+    author: "Author",
+    year: "Year",
+    genre: "Genre"
+};
+
+Book.propTypes = {
+    title: PropTypes.string,
+    author: PropTypes.string,
+    year: PropTypes.number,
+    genre: PropTypes.string
+}
+
+export default Book;
---
+++ src/Bookshelves.js
@@ -0,0 +1,123 @@
+import React, {Component} from 'react';
+import '../css/Bookshelves.css';
+import Book from './Book';
+
+
+class Bookshelves extends Component {
+    constructor() {
+        super(); 
+    //     this.state = {
+    //         book: [
+    //             {
+    //                 title:"The Notebook" ,
+    //                 author:"Nicholas Sparks", 
+    //                 year:"1996" ,
+    //                 genre:"Fiction"
+    //             },
+    //             {
+    //                 title:"The Red Scrolls" ,
+    //                 author:"Cassandra Clare" ,
+    //                 year:"2019" ,
+    //                 genre:"Fiction"
+    //             },
+    //             {
+    //                 title:"Game of Bones" ,
+    //                 author:"Carolyn Haines" ,
+    //              year:"2019" ,
+    //              genre:"Mystery"
+    //             },
+    //             { 
+    //                 title:"Welcome to Hell", 
+    //                 author:"Fah",
+    //                 year:"2000",
+    //                 genre:"Science Fiction"
+    //             },
+    //             {
+    //                  title:"Harry Potter", 
+    //                 author:"J.K. Rowling",
+    //                 year:"2001", 
+    //                 genre:"Fiction"
+    //             },
+    //             {
+    //                  title:"Moon Rising", 
+    //                 author:"Ian McDonald",
+    //                  year:"2019",
+    //                   genre:"Science Fiction"
+    //             }
+                
+    //         ]
+    //     }
+    // }
+
+    // addBook(){
+    //     this.state.book.push(
+    //         {
+    //             title: "New Book Title",
+    //             Author: "New Book Author",
+    //             Year: "New Book Year"
+                
+    //         }
+    //     );
+    //     this.setState(
+    //         {
+    //             book: this.state.book
+    //         }
+    //     )
+        
+    // }
+
+    this.state = {
+        books: []
+      };
+    }
+  
+    addBook() {
+      this.state.books.push({
+        id: Date.now()
+      });
+      this.setState({
+        books: this.state.books
+      });
+    }
+  
+    deleteBook(id) {
+      let newBookArr = this.state.books;
+      newBookArr.map((book, index) => {
+        if (id === book.id) {
+          newBookArr.splice(index, 1);
+        }
+      });
+      this.setState({
+        books: newBookArr
+      });
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="div-board">
+                    <div className="row">
+                    {this.state.books.map(book => {
+                    return (
+                        <Book
+                        key={book.id}
+                        id={book.id}
+                        deleteHandler={this.deleteBook.bind(this)}
+                        />
+                    );
+                    })}
+                   
+                     <div>
+                <button className="btn btn-success add-button" onClick ={this.addBook.bind(this)}>Add</button>
+            </div>
+                    </div>
+                </div>
+                <div>
+                    
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Bookshelves;
